de8a7c5c3446bad55a11a520a82bc01b9ea7694e
ghareth <gareth.boyes.13@ucl.ac.uk>
2016-02-29 09:19:04 +0000

    Workaround for bug in Papers that exports BibTeX files with % without backslash

--- bibulous.py
+++ bibulous.py
@@ -425,12 +425,17 @@ class Bibdata(object):
         for line in filehandle:
             self.i += 1
 
-            #Workaround for bug in Papers3 that includes a stray backslash when using ampersands      
-            line = line.replace(r'\{\&}',r'{\&}')
-
             ## Ignore empty and comment lines.
             if not line: continue
             if line.strip().startswith('%'): continue
+            
+            #Workaround for bug in Papers3 that includes a stray backslash when using ampersands      
+            line = line.replace(r'\{\&}',r'{\&}')
+        
+            #Workaround for bug in Papers3 that exports BibTeX with non-escaped percentage signs
+            percent = re.compile (r'(?<!\\)%', re.UNICODE)
+            line = re.sub(percent, r"\%", line)               
+            
             if line.startswith('}') and entry_brace_level == 1:
                 ## If a line *starts* with a closing brace, then assume the intent is to close the current entry.
                 entry_brace_level = 0
